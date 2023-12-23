32870be58f6224f390c7f5a18b46815a4a13b947
zane-neo <zaniu@amazon.com>
2023-12-23 11:02:21 +0800

    format file
    Signed-off-by: zane-neo <zaniu@amazon.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -81,4 +81,3 @@ cp ${distributions}/*.zip ./$OUTPUT/plugins
 ./gradlew publishPluginZipPublicationToZipStagingRepository -Dopensearch.version=$VERSION -Dbuild.snapshot=$SNAPSHOT -Dbuild.version_qualifier=$QUALIFIER
 mkdir -p $OUTPUT/maven/org/opensearch
 cp -r ./build/local-staging-repo/org/opensearch/. $OUTPUT/maven/org/opensearch
-
